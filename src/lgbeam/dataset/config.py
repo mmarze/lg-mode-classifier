d92d94eaac04b150fd7f5f806a9a630cbc43b23a
mmarze <marcinmarzejon@proton.me>
2026-08-11 15:37:54 +0200

    Changes in parematers for dataset generation

--- src/lgbeam/dataset/config.py
+++ src/lgbeam/dataset/config.py
@@ -6,14 +6,32 @@ LG_MODES = [
 
 WAVELENGTH = 529e-9
 
-IMAGE_SIZE = (2048, 2048)
+SENSOR_SIZE = (4512, 4576)
 
 PIXEL_SIZE = 2.74e-6
 
 CENTER_SHIFT = 50
 
-EXPOSURE = 5e-3
+EXPOSURE = 5e-6
 
-POWER_RANGE = (1e-4, 1e-5)
+QE = 0.68
+
+READ_NOISE = 2.3
+
+DARK_CURRENT = 22
+
+FULL_WELL = 9_400
+
+ADC_BITS = 12
+
+POWER_RANGE = (FULL_WELL//2, FULL_WELL)
 
 W0_RANGE = (200e-6, 800e-6)
+
+SEED = None
+
+L = 2e-3
+# Create bigger image -> shifting realized by cropping the image
+N = max(SENSOR_SIZE) + 2 * CENTER_SHIFT
+
+N_IMAGES_PER_CLASS = 1 # 9000
